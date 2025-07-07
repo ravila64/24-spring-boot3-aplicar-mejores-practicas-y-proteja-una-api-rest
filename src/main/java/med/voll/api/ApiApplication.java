9f1cf6e64a2a4f0068b329eded406b0b9b05b8e8
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-07-07 10:57:12 -0500

    detalle 1 solo medico+cambio pwd properties

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -9,5 +9,8 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
+	//@Override
+	public void run(String... args) throws Exception {
+		System.out.println("welcome app with spring boot 3, aplicar mejores practicas y proteger API");
+	}
 }
--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -23,16 +23,13 @@ public class MedicoController {
     public ResponseEntity registrar(@RequestBody @Valid DatosRegistroMedico datos, UriComponentsBuilder uriComponentsBuilder) {
         var medico = new Medico(datos);
         repository.save(medico);
-
         var uri = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
-
         return ResponseEntity.created(uri).body(new DatosDetalleMedico(medico));
     }
 
     @GetMapping
     public ResponseEntity<Page<DatosListaMedico>> listar(@PageableDefault(size=10, sort={"nombre"}) Pageable paginacion) {
         var page = repository.findAllByActivoTrue(paginacion).map(DatosListaMedico::new);
-
         return ResponseEntity.ok(page);
     }
 
@@ -41,7 +38,6 @@ public class MedicoController {
     public ResponseEntity actualizar(@RequestBody @Valid DatosActualizacionMedico datos) {
         var medico = repository.getReferenceById(datos.id());
         medico.actualizarInformaciones(datos);
-
         return ResponseEntity.ok(new DatosDetalleMedico(medico));
     }
 
@@ -50,14 +46,12 @@ public class MedicoController {
     public ResponseEntity eliminar(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
         medico.eliminar();
-
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity detallar(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
-
         return ResponseEntity.ok(new DatosDetalleMedico(medico));
     }
 }
