497ac7369da9108639a9520871a1d25de0fc7fc8
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-07-10 13:14:44 -0500

    generar token, uso, controller, service, properities, value

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DatosAutenticacion;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DatosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,15 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class AutenticacionController {
 
+   @Autowired   // inyeccion de dependencias
+   private TokenService tokenService;
+
    @Autowired
    private AuthenticationManager manager;
 
    @PostMapping
    public ResponseEntity iniciarSesion(@RequestBody @Valid DatosAutenticacion datos){
-      var token = new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasena());
-      var autenticacion = manager.authenticate(token);
-
-      return ResponseEntity.ok().build();
+      var authenticationToken = new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasena());
+      var autenticacion = manager.authenticate(authenticationToken);
+      var tokenJWT = tokenService.generarToken((Usuario) autenticacion.getPrincipal());
+      return ResponseEntity.ok(new DatosTokenJWT(tokenJWT));
+      //return ResponseEntity.ok("123456"); // ResponseEntity.ok().build();
    }
 
 }
---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,42 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+   @Value("${api.security.token.secret}")
+   private String secret;  // properties
+
+   public String generarToken(Usuario usuario){
+      // este algoritmo se busca en jwt.io / java / github / buscar create token
+      try {
+         //Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
+         var algoritmo = Algorithm.HMAC256(secret);
+         return JWT.create()  // se quito la asignacion al token y se dejo return
+               .withIssuer("API Voll.med")  // auth0, empresa firma el token
+               .withSubject(usuario.getLogin())
+               .withExpiresAt(fechaExpiracion())
+               //.withClaim("id", usuario.getId()) // usuarios llave valor
+               // se pueden colocar varios
+               .sign(algoritmo);
+      } catch (JWTCreationException exception) {
+         throw new RuntimeException("error al generar el token JWT", exception);
+      }
+   }
+
+   // example:expira 2 horas despues desde que se reciba el token
+   private Instant fechaExpiracion() {
+      return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));  // utc:-05:00 colombia, bog, lima, quito
+   }
+}
