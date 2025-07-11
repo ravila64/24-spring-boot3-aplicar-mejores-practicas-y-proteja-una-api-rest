94f15cf4f5744c6499c9e95dad2d494d6c2edec9
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-07-11 15:32:00 -0500

    generacion del subject

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,10 +13,15 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+   @Autowired
+   private TokenService tokenService;
+
    @Override
    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
       var tokenJWT = recuperarToken(request);
-      System.out.println("tokenJWT "+tokenJWT);
+      System.out.println("tokenJWT-> "+tokenJWT);
+      var subject = tokenService.getSubject(tokenJWT);
+      System.out.println("token subject "+subject);
       filterChain.doFilter(request, response);
    }
 
--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,7 +3,9 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
+import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToFile;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
@@ -23,13 +25,16 @@ public class TokenService {
       try {
          //Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
          var algoritmo = Algorithm.HMAC256(secret);
-         return JWT.create()  // se quito la asignacion al token y se dejo return
+         System.out.println("algoritmo "+algoritmo);
+         var token = JWT.create()  // se quito la asignacion al token y se dejo return
                .withIssuer("API Voll.med")  // auth0, empresa firma el token
                .withSubject(usuario.getLogin())
                .withExpiresAt(fechaExpiracion())
                //.withClaim("id", usuario.getId()) // usuarios llave valor
                // se pueden colocar varios
                .sign(algoritmo);
+         System.out.println("token en GenerarToken "+token);
+         return token;
       } catch (JWTCreationException exception) {
          throw new RuntimeException("error al generar el token JWT", exception);
       }
@@ -37,6 +42,26 @@ public class TokenService {
 
    // example:expira 2 horas despues desde que se reciba el token
    private Instant fechaExpiracion() {
-      return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));  // utc:-05:00 colombia, bog, lima, quito
+      return LocalDateTime.now().plusHours(72).toInstant(ZoneOffset.of("-05:00"));  // utc:-05:00 colombia, bog, lima, quito
    }
+
+   // validar el token enviado en el subject
+   public String getSubject (String tokenJWT) {
+      try {
+         var algoritmo = Algorithm.HMAC256(secret);
+         //Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
+         return JWT.require(algoritmo)
+               // specify any specific claim validations
+               .withIssuer("API Voll.med")  // estaba autho, servidor, quien desarrolla API
+               // reusable verifier instance
+               .build()
+               .verify(tokenJWT)
+               .getSubject();
+         //decodedJWT = verifier.verify(token);
+      } catch (JWTVerificationException exception){
+         // Invalid signature/claims
+         throw new RuntimeException("Token JWT invalido o expirado.");
+      }
+   }
+
 }
